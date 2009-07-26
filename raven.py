285e6c77eccdfcf9021a63677858430d942f0e46
Joseph Perla <donotrealemailme @ OR @@@ jperla.com>
2009-07-26 12:42:21 -0400

    makes a matrix, and licensed

--- raven.py
+++ raven.py
@@ -1,72 +1,184 @@
-
+#!/usr/bin/env python
+import math
+import StringIO
+import itertools
 import abc
+from contextlib import contextmanager
 
-class Figure:
-    __metaclass__ = abc.ABCMeta
-    @abstractmethod
-    def render():
-        pass
-
-class FeatureFigure(Figure):
-    __metaclass__ = abc.ABCMeta
-    def __init__(self, features, configuration):
-        assert(len(features) == len(configuration))
-        self.features = features
-        self.configuration = configuration
-    
-    @classmethod
-    def transform(cls, figure, amounts):
-        assert(len(amounts) == len(figure.configuration))
-        for a,f,c in zip(amounts, figure.features, figure.configurations):
-            if a > 0:
-                assert(isinstance(f, TransformableFeature))
-                assert(f.can_transform(c, a))
-        new_configurations = [f.transform(c, a) 
-                                        for f,c,a in zip(figure.features, 
-                                                         figure.configurations,
-                                                         amounts)]
-        new_figure = cls.__init__(figure.features, new_configuration)
-        return new_figure
-
-class OneSimpleFigure(FeatureFigure):
-    def __init__(self, features, configuration):
-        assert(len(features) == 1)
-        assert(isinstance(features[0], DrawableFeature))
-        FeatureFigure.__init__(self, features, configuration)
-        
-    def render():
-        feature = self.features[0][self.configuration[0]]
+import cairo
 
 class Feature(object):
     __metaclass__ = abc.ABCMeta
 
 class DrawableFeature(Feature):
     __metaclass__ = abc.ABCMeta
-    @abstractmethod
-    def draw():
+    @abc.abstractmethod
+    def draw(self, ctx):
         pass
+
+default_line_width = (3, 3)
 
 class ShapeFeature(DrawableFeature):
     __metaclass__ = abc.ABCMeta
-    #TODO: jperla: fill this in
+    def __init__(self, center=(.5,.5), 
+                       radius=.25, 
+                       rotation=0,
+                       mirror=False,
+                       shear=1.0,
+                       color=(0,0,0,1), 
+                       line_width=default_line_width):
+        self.center = center
+        self.radius = radius
+        self.rotation = rotation
+        self.mirror = mirror
+        self.shear = shear
+        self.color = color
+        self.line_width = line_width
 
-class Triangle(ShapeFeature):
-    pass
+    @contextmanager
+    def fat_scale(self, cr):
+        cr.save()
+        cr.translate((1.0 - self.fatness) / 2, 0)
+        cr.scale(self.fatness, 1.0)
+        yield
+        cr.restore()
 
-class Square(ShapeFeature):
-    pass
+    @contextmanager
+    def reflected(self, cr):
+        if self.mirror:
+            cr.save()
+            cr.translate(.5, .5)
+            cr.transform(cairo.Matrix(-1,0,0,1))
+            cr.translate(-.5, -.5)
+            yield
+            cr.restore()
+        else:
+            yield
 
-class Circle(ShapeFeature):
-    pass
+    @contextmanager
+    def rotated(self, cr):
+        cr.save()
+        cr.translate(.5, .5)
+        cr.rotate(self.rotation)
+        cr.translate(-.5, -.5)
+        yield
+        cr.restore()
+
+
+class OblongShapeFeature(ShapeFeature):
+    __metaclass__ = abc.ABCMeta
+    def __init__(self, center=(.5,.5), 
+                       radius=.25, 
+                       rotation=0.0,
+                       mirror=False,
+                       shear=1.0,
+                       color=(0,0,0,1), 
+                       line_width=default_line_width,
+                       fatness=1.0):
+        self.fatness = fatness
+        ShapeFeature.__init__(self, center, radius, rotation, mirror, shear, color, line_width)
+
+class FillableShapeFeature(ShapeFeature):
+    def __init__(self, center=(.5,.5), 
+                       radius=.25, 
+                       rotation=0.0,
+                       mirror=False,
+                       shear=1.0,
+                       color=(0,0,0,1), 
+                       line_width=default_line_width,
+                       fill=False,
+                       fill_color=(0,0,0,1)):
+        self.fill = fill
+        self.fill_color = fill_color
+        ShapeFeature.__init__(self, center, radius, rotation, mirror, shear, color, line_width)
+
+class OblongFillableShapeFeature(FillableShapeFeature, OblongShapeFeature):
+    def __init__(self, center=(.5,.5), 
+                       radius=.25, 
+                       rotation=0.0,
+                       mirror=False,
+                       shear=1.0,
+                       color=(0,0,0,1), 
+                       line_width=default_line_width,
+                       fatness=1.0,
+                       fill=False,
+                       fill_color=(0,0,0,1)):
+        OblongShapeFeature.__init__(self, center, radius, rotation, mirror, shear, color, line_width, fatness)
+        FillableShapeFeature.__init__(self, center, radius, rotation, mirror, shear, color, line_width, fill, fill_color)
+    
+class Triangle(OblongFillableShapeFeature):
+    def draw(self, cr):
+        cr.set_line_width(max(cr.device_to_user_distance(*self.line_width)))
+        cr.set_line_join(cairo.LINE_JOIN_ROUND)
+        if self.fill:
+            cr.set_source_rgba(*self.fill_color)
+            self.draw_lines(cr)
+            cr.fill()
+        cr.set_source_rgba(*self.color)
+        with self.reflected(cr):
+            with self.rotated(cr):
+                self.draw_lines(cr)
+        cr.stroke()
+
+    def draw_lines(self, cr):
+        x,y = self.center
+        xradius = self.radius * self.fatness
+        yradius = self.radius
+        r32 = (math.sqrt(3) / 2 * xradius)
+        left, right = x - r32, x + r32
+        bottom, top = y + (yradius / 2.0), y - yradius
+        cr.move_to (left, bottom)
+        cr.line_to ((left + right) / 2.0, top) # Line to (x,y)
+        cr.line_to (right, bottom) # Line to (x,y)
+        cr.close_path ()
+
+class Square(OblongFillableShapeFeature):
+    def draw(self, cr):
+        cr.set_line_width(max(cr.device_to_user_distance(*self.line_width)))
+        if self.fill:
+            cr.set_source_rgba(*self.fill_color)
+            self.draw_lines(cr)
+            cr.fill()
+        cr.set_source_rgba(*self.color)
+        with self.reflected(cr):
+            with self.rotated(cr):
+                self.draw_lines(cr)
+        cr.stroke()
+
+    def draw_lines(self, cr):
+        x,y = self.center
+        xradius = self.radius * self.fatness
+        yradius = self.radius
+        left, top = x - xradius, y - yradius
+        cr.set_line_join(cairo.LINE_JOIN_ROUND)
+        cr.rectangle(left, top, 2 * xradius, 2 * yradius)
+
+class Circle(OblongFillableShapeFeature):
+    def draw(self, cr):
+        cr.set_line_width(max(cr.device_to_user_distance(*self.line_width)))
+        if self.fill:
+            cr.set_source_rgba(*self.fill_color)
+            with self.fat_scale(cr):
+                self.draw_lines(cr)
+            cr.fill()
+        cr.set_source_rgba(*self.color)
+        with self.fat_scale(cr):
+            self.draw_lines(cr)
+        cr.stroke()
+
+    def draw_lines(self, cr):
+        x,y = self.center
+        cr.move_to(x + self.radius, y)
+        cr.arc(x, y, self.radius, 0, 2 * math.pi)
+        cr.close_path()
 
 class FeatureSet(object):
     __metaclass__ = abc.ABCMeta
+    def __init__(self):
+        pass
 
 class TransformableFeatureSet(FeatureSet):
     __metaclass__ = abc.ABCMeta
-    
-    def __init__(self, features):
-        self.features = features
 
     def transform(self, configuration, amount):
         return configuration + amount
@@ -76,7 +188,10 @@ class TransformableFeatureSet(FeatureSet):
 
 class DrawableFeatureSet(TransformableFeatureSet):
     __metaclass__ = abc.ABCMeta
-    #TODO: jperla: assert all drawable
+    def __init__(self, features):
+        TransformableFeatureSet.__init__(self)
+        for f in features:
+            assert(issubclass(f, DrawableFeature))
 
 class RingFeatureSet(TransformableFeatureSet):
     __metaclass__ = abc.ABCMeta
@@ -89,27 +204,144 @@ class RingFeatureSet(TransformableFeatureSet):
             return t - len(self.features)
 
     def can_transform(self, configuration, amount):
-        return 0 < amount < len(configuration)
+        return 0 < amount and 0 <= configuration
 
 class TripleFeatureSet(RingFeatureSet):
     __metaclass__ = abc.ABCMeta
-    def __init__(self, configurations):
-        assert(len(configurations) == 3)
-        RingFeature.__init__(self, configurations)
+    def __init__(self, features):
+        assert(len(features) == 3)
+        RingFeatureSet.__init__(self)
 
 class TripleDrawableFeatureSet(TripleFeatureSet, DrawableFeatureSet):
     __metaclass__ = abc.ABCMeta
-    #TODO: jperla: merge these
+    def __init__(self, features):
+        TripleFeatureSet.__init__(self, features)
+        DrawableFeatureSet.__init__(self, features)
 
 class ShapeFeatureSet(DrawableFeatureSet):
     __metaclass__ = abc.ABCMeta
-    #TODO: jperla: assert everything shape
+    def __init__(self, features):
+        DrawableFeatureSet.__init__(self, features)
+        for f in features:
+            assert(issubclass(f, ShapeFeature))
 
 class TripleShapeFeatureSet(TripleFeatureSet, ShapeFeatureSet):
-    #TODO: jperla: merge these
+    def __init__(self, features):
+        TripleFeatureSet.__init__(self, features)
+        ShapeFeatureSet.__init__(self, features)
+        self.features = features
+
+class Figure:
     __metaclass__ = abc.ABCMeta
+    @abc.abstractmethod
+    def render():
+        pass
+
+class FeatureFigure(Figure):
+    __metaclass__ = abc.ABCMeta
+    def __init__(self, feature_sets, features):
+        assert(len(feature_sets) == len(features))
+        self.feature_sets = feature_sets
+        #TODO: jperla: make sure features match up with feature sets
+        self.features = features
+    
+    def transform(self, configuration,  amounts):
+        assert(len(amounts) == len(configuration))
+        for a,fs,f,c in zip(amounts, self.feature_sets, self.features, configuration):
+            if a > 0:
+                assert(issubclass(fs, TransformableFeatureSet))
+                assert(fs(f).can_transform(c, a))
+        new_configuration = [fs(f).transform(c, a) 
+                                        for fs,f,c,a in zip(self.feature_sets,
+                                                            self.features,
+                                                            configuration,
+                                                            amounts)]
+        return new_configuration
+
+    @abc.abstractmethod
+    def render(self, configuration):
+        num_feature_sets = len(self.feature_sets)
+        assert len(self.features) == num_feature_sets == len(configuration)
+
+class CairoFigure(Figure):
+    def create_context(self, width, height, color=cairo.FORMAT_ARGB32):
+        surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, width, height)
+        cr = cairo.Context (surface)
+        cr.scale (width/1.0, height/1.0) # Normalizing the canvas
+        return surface, cr
+
+    def surface_to_png(self, surface):
+        buffer = StringIO.StringIO()
+        surface.write_to_png(buffer)
+        png = buffer.getvalue()
+        buffer.close()
+        return png
+
+class OneSimpleFigure(FeatureFigure, CairoFigure):
+    def __init__(self, feature_sets, features):
+        assert(len(feature_sets) == 1)
+        assert(issubclass(feature_sets[0], DrawableFeatureSet))
+        FeatureFigure.__init__(self, feature_sets, features)
+        
+    def render(self, configuration):
+        FeatureFigure.render(self, configuration)
+        surface, cr = self.create_context(128, 128)
+        drawable = self.features[0][configuration[0]]()
+        drawable.draw(cr)
+        return self.surface_to_png(surface)
+
+def rpm_from_pngs(pngs):
+    rpm = cairo.ImageSurface
+    width, height = 384,384
+    surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
+    cr = cairo.Context(surface)
+    for x,y,png in zip([0, 128, 256] * 3, [0] * 3 + [128] * 3 + [256] * 3, pngs):
+        figure = cairo.ImageSurface.create_from_png(StringIO.StringIO(png))
+        cr.set_source_surface(figure, x, y)
+        cr.paint()
+    buffer = StringIO.StringIO()
+    surface.write_to_png(buffer)
+    png = buffer.getvalue()
+    buffer.close()
+    return png
+
+def rpm_from_cmatrix(f, cmatrix):
+    pngs = [f.render(c) for c in itertools.chain(*cmatrix)]
+    return rpm_from_pngs(pngs)
+
+def cmatrix_from_one_transition(figure, configuration, transition):
+    c = [configuration]
+    for i in xrange(0,8):
+        c.append(figure.transform(c[i], transition))
+    cmatrix = [[c[0], c[1], c[2]], [c[3], c[4], c[5]], [c[6], c[7], c[8]]]
+    return cmatrix
+
+def cmatrix_from_two_transitions(figure, configuration, transition1, transition2):
+    f = figure
+    cmatrix = [[None,None,None], [None,None,None], [None,None,None]]
+    cmatrix[0][0] = configuration
+    c = cmatrix[0][0]
+    for i in xrange(1,3):
+        c = f.transform(c, transition2)
+        cmatrix[i][0] = c
+    for i in xrange(3):
+        c = cmatrix[i][0]
+        for j in xrange(1,3):
+            c = f.transform(c, transition1)
+            cmatrix[i][j] = c
+    for j in xrange(1,3):
+        c = cmatrix[0][j]
+        for i in xrange(1,3):
+            c = f.transform(c, transition2)
+            assert(cmatrix[i][j] == c)
+    return cmatrix
     
 
-def render(feature_sets, features, configurations):
-    #TODO: jperla: do the magic
-    pass
+        
+
+
+if __name__ == '__main__':
+    f = OneSimpleFigure([TripleShapeFeatureSet,], 
+                        [[Triangle, Square, Circle],])
+    png = f.render([2,])
+
--- web_raven.py
+++ web_raven.py
@@ -1,29 +1,78 @@
+#!/usr/bin/env python
+from raven import *
 import webify
 from webify.templates.helpers import html
 
-
-# Middleware
-from webify.middleware import install_middleware, EvalException, SettingsMiddleware
+from webify.middleware import EvalException, SettingsMiddleware
 
 app = webify.defaults.app()
 
 @app.subapp(u'/')
 @webify.urlable()
 def index(req, p):
-    
+    p(u'Hello, world!')
 
-# Server
+@app.subapp()
+@webify.urlable()
+def debug(req, p):
+    f = OneSimpleFigure([TripleShapeFeatureSet,],
+                                [[Triangle, Square, Circle],])
+    c = [2,]
+    cmatrix = cmatrix_from_one_transition(f, c, [1,])
+    cmatrix = cmatrix_from_two_transitions(f, c, [1,], [2,])
+    png = rpm_from_cmatrix(f, cmatrix)
+    p.headers = [webify.http.headers.content_types.image_png]
+    p(png)
+
+@app.subapp()
+@webify.urlable()
+def example(req, p):
+    WIDTH, HEIGHT = 256, 256
+
+    surface = cairo.ImageSurface (cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
+    ctx = cairo.Context (surface)
+
+    ctx.scale (WIDTH/1.0, HEIGHT/1.0) # Normalizing the canvas
+
+    pat = cairo.LinearGradient (0.0, 0.0, 0.0, 1.0)
+    pat.add_color_stop_rgba (1, 0.7, 0, 0, 0.5) # First stop, 50% opacity
+    pat.add_color_stop_rgba (0, 0.9, 0.7, 0.2, 1) # Last stop, 100% opacity
+
+    ctx.rectangle (0, 0, 1, 1) # Rectangle(x0, y0, x1, y1)
+    ctx.set_source (pat)
+    ctx.fill ()
+
+    ctx.translate (0.1, 0.1) # Changing the current transformation matrix
+
+    ctx.move_to (0, 0)
+    ctx.arc (0.2, 0.1, 0.1, -math.pi/2, 0) # Arc(cx, cy, radius, start_angle, stop_angle)
+    ctx.line_to (0.5, 0.1) # Line to (x,y)
+    ctx.curve_to (0.5, 0.2, 0.5, 0.4, 0.2, 0.8) # Curve(x1, y1, x2, y2, x3, y3)
+    ctx.close_path ()
+
+    ctx.set_source_rgb (0.3, 0.2, 0.5) # Solid color
+    ctx.set_line_width (0.02)
+    ctx.stroke ()
+
+    p.headers = [webify.http.headers.content_types.image_png]
+
+    import StringIO
+    s = StringIO.StringIO()
+    surface.write_to_png(s) # Output to PNG
+    png = s.getvalue()
+    
+    p(png)
+
 from webify.http import server
 if __name__ == '__main__':
     mail_server = webify.email.LocalMailServer()
     settings = {
-                'files_path': lib.location.Location('/home/jperla/Dropbox/         Labmeeting/Development/Docs/'),
                 'mail_server': mail_server
                }
 
-    wrapped_app = install_middleware(app, [
-                                            SettingsMiddleware(settings),
-                                            EvalException,
-                                          ])
+    wrapped_app = webify.wsgify(app, 
+                                        SettingsMiddleware(settings),
+                                        EvalException,
+                                     )
 
-    server.serve(wrapped_app, host='0.0.0.0', port=8080)
+    server.serve(wrapped_app, host='0.0.0.0', port=8087, reload=True)
